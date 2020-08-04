ae49c10b7002094c72d61dd5ae6b1119555c3553
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-08-04 10:05:58 -0400

    commiting for help

--- js/main.js
+++ js/main.js
@@ -9,7 +9,11 @@ let burgerButton = document.querySelector('#burgerButton'),
     nextAward = document.querySelector('.next'),
     awardName = document.querySelector('.awardName'),
     awardInfo = document.querySelector('.awardInfo'),
-    activeDot = document.querySelectorAll('.dot');
+    aromaType = document.querySelector('#aromaType'),
+    aromaWriteup = document.querySelector('.aromaWriteup'),
+    beerBottle = document.querySelector('#beerBottle'),
+    activeDot = document.querySelectorAll('.dot'),
+    aromaButton = document.querySelectorAll('.aromaButton');
 
 const awardWriteup = [
     [`Outstanding Olefactory Award`, `Awarded in Banff, Alberta, the Outstanding Olfactory Award is awarded to products that excel at "making the world smell better, one product at a time". It is an honour to be presented with this award as we take pride in creating a product that will provide fresh scents in the most exiting way. We share this award with our sister company, Renuzit® Brand Snuggle®, who have won many times in the past. Let it serve as our commitment to providing only the freshest scents through our beer!`],
@@ -17,12 +21,12 @@ const awardWriteup = [
     [`Brewery Of The Month Award`, `The prestigious Brewery Of The Year Award is given to breweries of the highest quality. With such competitors as Labatts, Coors, Molson, and the like, we were ecstatic to receive this award presented in Montreal, Quebec. While it's set the bar for our quality, it also motivates us to continue providing beer of the utmost quality, held to the highest standards. This award has put us on the world stage, and we intend on continues to provide those standards to the beer drinkers of the world!`]
 ]
 
-// initialize awards to show the first award
-function initAward() {
-mobileAwardPic.src = `images/award0.svg`
-awardName.textContent = `${awardWriteup[0][0]}`;
-awardInfo.textContent = `${awardWriteup[0][1]}`;
-}
+const aromaChoice = [
+    [`SuperFresh Original`, `Dash away stink and welcome in snuggly freshness with scents of clean laundry and bright floral notes. Make your beer make you SuperFresh, especially in smaller spaces like lounges and pubs, with BECK’S® & Renuzit® Snuggle® SuperFresh Original.`],
+    [`Linen Escape`, `Get the perks of laundry day without having to do any laundry. The BECK’S® & Renuzit® Snuggle® Linen Escape beer is perfectly made to fill your nose with the inviting scents of clean laundry and fresh floral notes.`],
+    [`Relaxing Lavender`, `Gone are the days you need to go to the spa to soothe your soul and feel sweet serenity. Transform your beer into a spa-like getaway by filling every piece of clothing with the relaxing aroma of lavender with BECK’S® & Renuzit® Snuggle® Relaxing Lavender.`],
+    [`Enchanting Tropical`, `Delight in bright tropical aromas all year round. Be swept away by the invigorating scents of enchanting tropics, making every refreshing sip smell like a vacation with BECK’S® & Renuzit® Snuggle® Enchanting Tropical. We know you’ll enjoy the vibrant scent experience!`]
+]
 
 //expand the burger menu on button click
 function burgerMenuExpand(){
@@ -43,33 +47,51 @@ function scrollToTop() {
     window.scrollTo(0, 0);
 }
 
+// initialize awards to show the first award
+function initAward() {
+    mobileAwardPic.src = `images/award0.svg`
+    awardName.textContent = `${awardWriteup[0][0]}`;
+    awardInfo.textContent = `${awardWriteup[0][1]}`;
+    }
+
 // advance text from awardWriteup
-function nextAwardSlide(i) {
-    for (let i = 0; i < awardWriteup.length; i++)
-    mobileAwardPic.src = `images/award${awardWriteup.length += i}.svg`
-    awardName.textContent = `${awardWriteup[awardWriteup.length += i]}`;
-    awardInfo.textContent = `${awardWriteup[awardWriteup.length += i]}`;
+function nextAwardSlide() {
+    for (let i = 0; i < awardWriteup.length; i++) {
+    mobileAwardPic.src = `images/award${[i]}.svg`;
+    awardName.textContent = `${awardWriteup[i][0]}`;
+    awardInfo.textContent = `${awardWriteup[i][1]}`;
+    }
 }
 
 // revert text and image
 function prevAwardSlide() {
-    awardName.textContent = `${awardWriteup[awardWriteup.length -= 1]}`;
-    awardInfo.textContent = `${awardWriteup[awardWriteup.length -= 1]}`;
+    for (let i = 0; i < awardWriteup.length; i--) {
+        mobileAwardPic.src = `images/award${[-i]}.svg`;
+        awardName.textContent = `${awardWriteup[i][0]}`;
+        awardInfo.textContent = `${awardWriteup[i][1]}`;
+    }
 }
-
 // choose award from dots below
 function chooseAward() {
     awardName.textContent = `${awardWriteup[this.data.offset][0]}`
 }
 
+// select aroma from each button
+function chooseAroma() {
+    aromaType.textContent = `${aromaChoice[this.dataset.aroma][0]}`;
+    aromaWriteup.textContent = `${aromaChoice[this.dataset.aroma][1]}`;
+    beerBottle.src = `images/bottle${this.dataset.aroma}.png`;
+}
+
 
 // event handelers 
+window.addEventListener('load', initAward);
 window.addEventListener('scroll', scrollClose);
 burgerButton.addEventListener('click', burgerMenuExpand);
 toTopButton.addEventListener('click', scrollToTop);
 nextAward.addEventListener('click', nextAwardSlide);
 prevAward.addEventListener('click', prevAwardSlide);
-
 activeDot.forEach(dot => dot.addEventListener('click', chooseAward));
+aromaButton.forEach(button => button.addEventListener('click', chooseAroma));
 })();
 
